eaac7bc01c6d913ae27d1faac7fba34a20889cbb
Michael <mihai.mihalachi03@gmail.com>
2023-11-16 12:09:49 +0200

    First commit

---
+++ src/main/java/com/endava/app/RhythmiqApplication.java
@@ -0,0 +1,12 @@
+package com.endava.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhythmiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhythmiqApplication.class, args);
+	}
+}
